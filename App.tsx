899f9807e906d2264445c373ecbbcff2823712ae
Marwan-Zakia <marwanamir.ma@gmail.com>
2022-03-20 17:32:18 +0300

    fix for the .env

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>suc</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { AppRegistry } from "react-native";
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+} from "@apollo/client";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+// Initialize Apollo Client
+const client = new ApolloClient({
+	uri: "localhost:19002/graphql",
+	cache: new InMemoryCache(),
 });
+
+const App = () => (
+	<ApolloProvider client={client}>
+		<View style={styles.container}>
+			<Text>sucss</Text>
+			<StatusBar style="auto" />
+		</View>
+	</ApolloProvider>
+);
+
+export default App;
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
+AppRegistry.registerComponent("MyApplication", () => App);
